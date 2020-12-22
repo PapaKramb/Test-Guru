ba8ac41df21cd2ad6a005af87d72c4f568df693d
PapaKramb <guccigang5554@gmail.com>
2020-12-22 20:25:32 +0900

    Add seeds and update Test and User model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,17 @@
 class Test < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  has_many :questions
+
+  scope :easy, -> { by_level(0..1) }
+  scope :medium, -> { by_level(2..4) }
+  scope :hard, -> { by_level(5..Float::INFINITY) }
+
+  scope :by_level, -> (level) { where(level: level) }
+
+  scope :by_category, -> (category) { joins(:category).where(categories: { title: category }) }
+
+  def self.ordered_test_titles_by_category(category)
+    Test.by_category(category).order(title: :desc).pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  [
+    {name: Alexey, email: test@mail.com, password: 123456},
+    {name: Katya, email: test1@mail.com, password: 654321},
+    {name: Grogu, email: padavan@mail.com, password: qwerty}
+  ]
+)
+
+Category.create!(
+  [
+    {title: 'Ruby'},
+    {title: 'HTML'},
+    {title: 'CSS'}
+  ]
+)
+
+Test.create!(
+  [
+    {title: 'Ruby on Rails', category: Category.first, level: 3, user: User.first},
+    {title: 'HyperText Markup Language', category: Category.second, level: 2, user: User.second},
+    {title: 'Cascading Style Sheets', category: Category.third, level: 1, user: User.third}
+  ]
+)
+
+Question.create!(
+  [
+    {boby: 'ROR question 1', test: Test.first},
+    {boby: 'HTML question 1', test: Test.second},
+    {boby: 'CSS question 1', test: Test.third}
+  ]
+)
+
+Answer.create!(
+  [
+    {body: 'Answer A', question: Question.first, correct: true},
+    {body: 'Answer B', question: Question.first},
+    {body: 'Answer C', question: Question.first},
+    {body: 'Answer A', question: Question.second},
+    {body: 'Answer B', question: Question.second, correct: true},
+    {body: 'Answer C', question: Question.second},
+    {body: 'Answer A', question: Question.third},
+    {body: 'Answer B', question: Question.third},
+    {body: 'Answer C', question: Question.third, correct: true},
+  ]
+)
