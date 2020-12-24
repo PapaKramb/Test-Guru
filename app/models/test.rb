25a2d110144ee44b24290425e9987339458bacb0
PapaKramb <guccigang5554@gmail.com>
2020-12-24 23:37:00 +0900

    Add method join for User and Test models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   # Метод, который возвращает отсортированный по убыванию массив названий всех Тестов
-  def return_title_tests_categories(category)
-    joins(:category).where("categories.title = ?", category).order(title: :desc).pluck(:title)
+  def self.return_title_tests_categories(category)
+    joins('JOIN categories ON tests.category_id = categories.id').where(categories: {title: category}).order(title: :desc)
   end
 end
