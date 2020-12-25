76d5e428e7c259ec615f4336bb7d8723315092c5
PapaKramb <guccigang5554@gmail.com>
2020-12-26 02:58:40 +0900

    Update where-method for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def user_tests_level(level)
-    Test.joins("INNER JOIN completed_tests ON completed_tests.test_id = tests.id").where(tests: {level: level}).pluck(:level)
+    Test.joins("INNER JOIN completed_tests ON completed_tests.test_id = tests.id").where(completed_tests: {user_id: id}, tests: {level: level}).pluck(:level)
   end
 end
