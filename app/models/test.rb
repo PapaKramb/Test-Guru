4f0cc3a2396994d4a31142a44a9548e50bd1c75e
PapaKramb <guccigang5554@gmail.com>
2020-12-27 04:10:37 +0900

    Refactoring Test and User models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
-  belongs_to :users
+  belongs_to :author, class_name: "User", optional: true
   belongs_to :category
   has_many :questions, dependent: :destroy
   has_many :completed_tests, dependent: :destroy
-  has_many :users, through: :completed_tests, dependent: :destroy
+  has_many :users, through: :completed_tests
 
   def self.title_tests_categories(category)
     joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
