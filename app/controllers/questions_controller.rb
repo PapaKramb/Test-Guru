ebc18ba7e7f267c18647cfce61ffc9646732b7f7
PapaKramb <guccigang5554@gmail.com>
2021-01-11 20:13:04 +0900

    Following the principles REST

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,49 @@
+class QuestionsController < ApplicationController
+  before_action :find_test
+  before_action :find_question, only: %i[destroy show]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render json: @test.questions
+  end
+
+  def show
+    render json: @question
+  end
+
+  def new 
+  end
+
+  def create
+    @question = @test.questions.new(questions_params)
+
+    if @question.save
+      redirect_to test_questions_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy!
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def questions_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found'
+  end
+end
