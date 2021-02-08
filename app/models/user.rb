98dca3da79077a933b5b48615f2db57c23fccbdb
PapaKramb <guccigang5554@gmail.com>
2021-02-08 19:50:18 +0900

    Add authenticate for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :completed_tests
   has_many :tests, through: :completed_tests
   has_many :authored_tests, class_name: 'Test'
 
-  validates :name, presence: true
-  validates :email, uniqueness: true, presence: true
-  validates :password, length: { in: 6..20 }, presence: true
+  has_secure_password
+
+  # validates :email, uniqueness: true, presence: true
+  # validates :password, length: { in: 6..20 }, presence: true, if: Proc.new { |u| u.password_digest.blank? }
+  # validates :password, confirmation: true
 
   def user_tests_level(level)
     tests.where(level: level)
@@ -14,4 +21,5 @@ class User < ApplicationRecord
   def completed_test(test)
     completed_tests.order(id: :desc).find_by(test: test)
   end
+
 end
