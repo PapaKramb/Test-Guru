027e769337b7513a7e46121640287a7bc322d0c4
PapaKramb <guccigang5554@gmail.com>
2021-02-11 01:44:05 +0900

    Rails Mailer

--- app/controllers/completed_tests_controller.rb
+++ app/controllers/completed_tests_controller.rb
@@ -1,4 +1,5 @@
 class CompletedTestsController < ApplicationController
+  
   before_action :authenticate_user!
   before_action :set_completed_test, only: %i[show update result]
 
@@ -10,6 +11,7 @@ class CompletedTestsController < ApplicationController
     @completed_test.accept!(params[:answer_ids])
     
     if @completed_test.completed?
+      TestsMailer.completed_test(@completed_test).deliver_now
       redirect_to result_completed_test_path(@completed_test)
     else
       render :show
