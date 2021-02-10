b6e10281418a17317488d8d38c8af076d15cea86
PapaKramb <guccigang5554@gmail.com>
2021-02-11 03:10:00 +0900

    Admin resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, exept: :index do
       resources :answers, shallow: true, exept: :index
     end
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     member do
       get :result
     end
+  end
+
+  namespace :admin do
+    resources :tests
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
