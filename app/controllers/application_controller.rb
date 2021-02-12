776f6ff948aabd0621d85b09df8ae9067006cb3a
PapaKramb <guccigang5554@gmail.com>
2021-02-13 01:26:22 +0900

    Fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    current_user.admin? ? admin_tests_path : super
+      if resource.is_a?(Admin)
+        admin_tests_path
+      else
+        super
+      end
   end
 
   protected
