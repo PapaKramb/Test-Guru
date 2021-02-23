4a1aedf93a1bcecf021d4f718f43b343969b55b2
PapaKramb <guccigang5554@gmail.com>
2021-02-23 16:10:37 +0900

    Data Attributes

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,10 +1,9 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[show edit update destroy start]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def new
     @test = Test.new
@@ -32,6 +31,14 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
 
@@ -39,6 +46,10 @@ class Admin::TestsController < Admin::BaseController
   end
 
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,7 @@ Rails.application.routes.draw do
       resources :answers, shallow: true, exept: :index
     end
 
-    member do
-      post :start
-    end
+      post :start, on: :member
 
   end
 
@@ -26,6 +24,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :tests do
+      patch :update_inline, on: :member
+
       resources :questions, except: :index, shallow: true do
         resources :answers, except: :index, shallow: true
       end
