e4554ea7dbbd26cc46fa8007ae685824cdd9951e
PapaKramb <guccigang5554@gmail.com>
2021-03-02 04:30:02 +0900

    Add before_action :authenticate_user! to badges_controller

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,11 +3,12 @@ class Badge < ApplicationRecord
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
-  enum rule: [:first_try, :all_tests_by_category, :all_tests_by_level]
+  enum rule: { first_try: 0, all_tests_by_category: 1, all_tests_by_level: 2 }
 
   validates :rule, presence: true, inclusion: { in: rules }
   validates :title, presence: true
   validates :image, presence: true
   validates :description, presence: true
-  
+  # enum BADGE_LIST: [:first_try, :all_tests_by_category, :all_tests_by_level]
+
 end
