1b7b84966493e78529cd6156569a8341254d0065
PapaKramb <guccigang5554@gmail.com>
2021-03-02 02:51:22 +0900

    Fix emun rules

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,10 +3,11 @@ class Badge < ApplicationRecord
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
+  enum rule: [:first_try, :all_tests_by_category, :all_tests_by_level]
+
+  validates :rule, presence: true, inclusion: { in: rules }
   validates :title, presence: true
-  validates :rule, presence: true
   validates :image, presence: true
   validates :description, presence: true
-  enum BADGE_LIST: [:first_try, :all_tests_by_category, :all_tests_by_level]
-
+  
 end
